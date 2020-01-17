d213546d0cb5d2ac7b2e8b0c63e38b2ee965f268
Lillian Wright <10803934@my.uvu.edu>
2020-01-17 14:09:21 -0700

    Attack Scripts
    Unfinished Attack Script and health script. Figure out attack instead of destroy.

---
+++ Game setup/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject player;
+    public void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Health>()
+        {
+            Destroy(player.gameObject);
+        }
+    }
+}
---
+++ Game setup/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+    
+    void Start()
+    {
+        health = 1;
+    }
+    
+    void Update()
+    {
+        
+    }
+}

2cacd9d3d0fd2ff35cf340c0ce5808cc6cad360d
Lillian Wright <10803934@my.uvu.edu>
2020-02-03 16:38:02 -0700

    Character Controller and scenes 2-4

--- New Unity Project/Assets/Scripts/Character/CharacterController.cs
+++ New Unity Project/Assets/Scripts/Character/CharacterController.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))
 public class CharacterController : MonoBehaviour
 {
+    private Vector3 position;
+    private CharacterController _controller;
+
+    public float moveSpeed = 10f; gravity = 9.81f, jumpSpeed = 30f;
+
+    public float rotateSpeed = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move(Input.GetAxis("Vertical"));
+        Rotate(Input.GetAxis("Horizontal"));
+    }
+    public void Move (float myInput)
+    {
+        position.x = myInput;
+        position = transform.TransformDirection(position * moveSpeed * Time.deltaTime);
+    }
+
+    void Rotate(float myInput)
+    {
+        transform.Rotate(0, myInput * rotateSpeed, 0);
     }
 }

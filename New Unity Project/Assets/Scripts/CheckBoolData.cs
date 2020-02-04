2d4a7b75ed1f0b74474b3eabef2885fb0e2194c2
Lillian Wright <10803934@my.uvu.edu>
2020-02-04 12:49:02 -0700

    bools and triggers

---
+++ New Unity Project/Assets/Scripts/CheckBoolData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBoolData : MonoBehaviour
+{
+    public BoolData boolDataObj;
+
+   
+    void Update()
+    {
+        if (boolDataObj.value)
+        {
+            print("true");
+            //work to do
+        }
+        else
+        {
+            //do false work
+        }
+    }
+}

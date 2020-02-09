bc1f2edcbf42c92900b04fec157ad76adc67ffd1
Lillian Wright <10803934@my.uvu.edu>
2020-02-08 20:04:23 -0700

    MoveTo script and setting up for NavMesh
    Enemie prefabs given MoveTo scripts to establish points they patrol from.

---
+++ New Unity Project/Assets/Scripts/Ai/MoveTo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    public void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}

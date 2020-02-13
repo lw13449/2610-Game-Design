aeb870c57aa848345af428e5f7d4e7263a2d8b14
Lillian Wright <10803934@my.uvu.edu>
2020-02-13 15:10:26 -0700

    Change scenes and door triggers

---
+++ New Unity Project/Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorSwitch : MonoBehaviour
+{
+    public GameObject door;
+    public UnityEvent eventAction;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        eventAction.Invoke();
+    }
+}

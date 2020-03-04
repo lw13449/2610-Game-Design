c80d8629dc1dc5b3c2821bbec4fc52f1cfe0ccb2
Lillian Wright <10803934@my.uvu.edu>
2020-03-04 14:59:26 -0700

    xp start and game action

---
+++ New Unity Project/Assets/Scripts/GameAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class GameAction : ScriptableObject
+{
+    public UnityAction actionEvent;
+
+    public void Raise()
+    {
+        actionEvent.Invoke();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/XPPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class XPPoint : ScriptableObject
+{
+    public UnityAction<int> upgradeAction;
+    
+    public int value = 10;
+
+    public void Upgrade()
+    {
+        upgradeAction?.Invoke(value);
+    }
+}

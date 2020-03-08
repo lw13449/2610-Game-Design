ca14ac77ccf8a300921d606887ee44e7894fafa7
Lillian Wright <10803934@my.uvu.edu>
2020-03-07 23:21:55 -0600

    spawning

---
+++ New Unity Project/Assets/Scripts/Spawning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject spawnObj;
+    public int spawnNumber = 1;
+    public int spawnMax = 3;
+
+    public void Start()
+    {
+        if (spawnNumber <= spawnMax)
+        {
+            Vector3 spawnPosition = transform.position;
+            Instantiate(spawnObj, spawnPosition, Quaternion.identity);
+        }
+
+        if (spawnNumber == spawnMax)
+        {
+            return;
+        }
+    }
+}

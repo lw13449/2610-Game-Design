bee360f54aeaffeebd8fe11652ece4360bd94280
Lillian Wright <10803934@my.uvu.edu>
2020-03-12 13:55:04 -0600

    platform script

---
+++ New Unity Project/Assets/Scripts/Character/AttachToPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class AttachToPlatform : MonoBehaviour
+{
+    //public NameID platformId;
+    //This goes on the player
+    private void OnTriggerEnter(Collider other)
+    {
+        //var nameIdObj = other.GetComponent<IdBehaviour>().nameIdObj;
+        //if (nameIdObj != null &&)
+        //{
+        //    transform.parent = other.transform;
+        //}
+        transform.parent = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}

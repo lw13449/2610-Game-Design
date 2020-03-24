9730d87084a5348abd5f513cb14a63ec91b3c8c3
Lillian Wright <10803934@my.uvu.edu>
2020-03-24 11:04:30 -0600

    Boss controller

---
+++ New Unity Project/Assets/Scripts/BossController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public CharacterStats bossStats;
+    public float lookRadius;
+    private Transform target;
+
+    private Animator anim;
+    public GameObject player;
+    public GameObject lastDoorSwitch;
+
+    public CharacterCombat combat;
+
+    public void Start()
+    {
+        target = player.transform;
+        player = GameObject.Find("Player");
+        combat = GetComponent<CharacterCombat>();
+    }
+
+    public void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        
+        if (distance <= lookRadius)
+        {
+            CharacterStats targetStats = target.GetComponent<CharacterStats>();
+            if (targetStats != null)
+            {
+                combat.Attack(targetStats);
+            }
+            FaceTarget();
+        }
+        if (bossStats.currentHealth <= 0)
+        {
+            Destroy(gameObject);
+            lastDoorSwitch.SetActive(true);
+        }
+    }
+    public void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime*5f);
+    }
+    public void TakeDamage(int damage)
+    {
+        //noise for attack when they take damage
+        //Instantiate(sparkEffect, transform.position, Quaternion.identity);
+        Debug.Log("damage Taken");
+        bossStats.currentHealth -= damage;
+    }
+}

270ce0cf453a23db6278f1a805d367041a643428
Lillian Wright <10803934@my.uvu.edu>
2020-04-09 18:02:45 -0600

    gameplay fix-its

--- New Unity Project/Assets/Scripts/DoorSwitch.cs
+++ New Unity Project/Assets/Scripts/DoorSwitch.cs
@@ -7,16 +7,20 @@ public class DoorSwitch : MonoBehaviour
     public GameObject door;
     public UnityEvent eventAction;
     public Animator switchAnimation;
+    public CharacterStats bossStats;
 
-    public void OnTriggerEnter(Collider other)
+    public void OpenDoor()
     {
         Debug.Log("Door is open.");
         door.transform.position = new Vector3(0, 10, 0);
-        switchAnimation.SetTrigger("Open Door");
+        switchAnimation.SetTrigger("On");
     }
 
     public void KillBoss()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //kill bossStats currenthealth with damage of 25.
+        }
     }
 }
